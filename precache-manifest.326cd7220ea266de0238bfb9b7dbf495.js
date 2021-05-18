64ba97d48317e7801a0e29e41bb69a7852de692b
anishs95 <anish.s95@gmail.com>
2021-05-18 16:18:14 +0530

    Updates

---
+++ precache-manifest.326cd7220ea266de0238bfb9b7dbf495.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfe27ede97c4a8fa6fe4c56c1a7b9181",
+    "url": "/anish/index.html"
+  },
+  {
+    "revision": "eb2dca3ab0ef0b715b4c",
+    "url": "/anish/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/anish/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/anish/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb2dca3ab0ef0b715b4c",
+    "url": "/anish/static/js/main.0b2c8a47.chunk.js"
+  },
+  {
+    "revision": "726ebca8a7097f67b906",
+    "url": "/anish/static/js/runtime-main.308b823a.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/anish/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/anish/static/media/resume.9d3da0e2.pdf"
+  }
+]);
